18f6a5ad3cba5d63cde2a793727211a5a5786aec
Elected21 <spushkov@yahoo.com>
2024-10-17 21:45:41 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Напиши любой текст: ')
+
+print('В твоём тексте', len(my_string) , 'символов')
+print( 'Твой текст большими буквами - ' , my_string .upper())
+print( 'Твой текст маленькими буквами - ' , my_string .lower())
+print( 'Твой текст без пробелов - ' , my_string .replace(' ', ''))
+print( 'Первая буква текста - ' , my_string [0])
+print( 'Последняя буква текста - ' , my_string [-1])
